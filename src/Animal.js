fd425ff0538ac9ef6f95efb84e0d648501cb9063
aharri64 <haveacompleteday@gmail.com>
2021-02-23 11:27:01 -0700

    added Animal.js

---
+++ src/Animal.js
@@ -0,0 +1,11 @@
+// * functional commponent
+
+function Animal() {
+    return (
+        <div>
+            <h1>Animal</h1>
+        </div>
+    )
+}
+
+export default Animal;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Animal from './Animal'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          YOOOOOOOOOOOOOOOO
-        </p>
-        <a
-          className="App-link"
-          href="https://nba.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Title</h1>
+      <Animal />
     </div>
   );
 }
