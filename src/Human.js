3689819d57901eb8d08d553b501cef45719ef521
aharri64 <haveacompleteday@gmail.com>
2021-02-23 12:14:31 -0700

    learned about nesting: added player and nested it into human which as nested in app"

--- src/Human.js
+++ src/Human.js
@@ -1,10 +1,12 @@
 // * My functional component
+import Player from "./Player";
 
 function Human(props) {
     return(
         <div>
             <h1>{props.name}</h1>
             <h3>They live in {props.location}</h3>
+            <Player position="forward"/>
         </div>
     )
 }
