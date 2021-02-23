86693fad128597f4246cc9723b7289b12968cc8c
aharri64 <haveacompleteday@gmail.com>
2021-02-23 11:11:25 -0700

    welcome to react

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          YOOOOOOOOOOOOOOOO
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://nba.com"
           target="_blank"
           rel="noopener noreferrer"
         >
